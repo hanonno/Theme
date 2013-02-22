c4c15aaec223fd42273940d65a72c09212bdbed8
Hanno ten Hoor <hanonno@gmail.com>
2013-02-22 10:53:37 +0100

    Initial Commit

---
+++ OMGTheme/OMGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OMGAppDelegate.h
+//  OMGTheme
+//
+//  Created by H.O. ten Hoor on 2/22/13.
+//  Copyright (c) 2013 H.O. ten Hoor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OMGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OMGTheme/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OMGTheme
+//
+//  Created by H.O. ten Hoor on 2/22/13.
+//  Copyright (c) 2013 H.O. ten Hoor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OMGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OMGAppDelegate class]));
+    }
+}
